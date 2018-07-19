177746a93e9eba683509c0ed0ba14d67190c6e32
ShawnBorn-Newton <shawn.newton3@gmail.com>
2018-07-18 23:07:04 -0400

    templates

--- first_app/views.py
+++ first_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('<em>Will This Work</em>')
+  my_dict = {'insert_me':"Hello form views!"}
+  return render(request,'first_app/index.html',context=my_dict)
